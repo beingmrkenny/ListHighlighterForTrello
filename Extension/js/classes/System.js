bbe730cd851edd7e358b1ac5b12bc65168819252
Mark Kenny <beingmrkenny@gmail.com>
2017-07-20 20:10:06 +0100

    Refactoring watch to be more self contained

--- Extension/js/classes/System.js
+++ Extension/js/classes/System.js
@@ -1,28 +1,23 @@
 class System {
 
 	static setup () {
-		watch (document.querySelector('title'), watchers.title);
+		watch ('title');
 		System.headerCardsSetup();
-		System.watchBoardForNewLists();
-		System.watchListTitleChanges();
+		watch ('board');
+		watch ('listTitle');
 		keepTrying(ListHighlighter.highlight, 5, 700);
 		System.detectAndSaveColorBlindFriendlyMode();
-		System.watchBodyClasslistForColorBlindFriendlyMode();
+		watch ('body');
 	}
 
 	static headerCardsSetup () {
 		// used to be a title watcher here
-		watch (document.querySelectorAll('.list-card-title'), watchers.listCardTitle);
-		watch (document.getElementById('board'), watchers.board);
-		watch (document.querySelectorAll('.list-cards'), watchers.list);
+		watch ('listCardTitle');
+		watch ('board');
+		watch ('list');
 	}
 
 	// TODO Does it need a destup method to get all observers and get rid of them
-
-	// body
-	static watchBodyClasslistForColorBlindFriendlyMode () {
-		watch (document.body, watchers.body);
-	}
 
 	// body
 	static detectAndSaveColorBlindFriendlyMode (mutationRecords) {
@@ -49,15 +44,10 @@ class System {
 
 	// title
 	static processPageOnTitleChange () {
-		System.watchBoardForNewLists();
-		System.watchListTitleChanges();
+		watch ('board');
+		watch ('listTitle');
 		ListHighlighter.highlight();
 		window.setTimeout (System.headerCardsSetup, 100);
-	}
-
-	// board
-	static watchBoardForNewLists() {
-		watch (document.querySelector('#board'), watchers.board);
 	}
 
 	// board
@@ -65,11 +55,11 @@ class System {
 
 	    var newList = mutationRecords[0].addedNodes[0];
 	    if (newList && newList.classList.contains('list-wrapper')) {
-	        watch (newList.querySelector('.list-cards'), watchers.list);
+	        watch ('list', newList.querySelector('.list-cards'));
 	    }
 
 		ListHighlighter.highlight();
-		System.watchListTitleChanges();
+		watch ('listTitle');
 	}
 
 	// list
@@ -78,16 +68,11 @@ class System {
 	        var newCard = mutationRecords[0].addedNodes[0];
 	        if (newCard && newCard.classList.contains('list-card')) {
 	            Card.processCards(newCard);
-	            watch (newCard.querySelector('.list-card-title'), watchers.listCardTitle);
+	            watch ('listCardTitle', newCard.querySelector('.list-card-title'));
 	        } else {
 	            Card.processCards(mutationRecords[0].target.querySelectorAll('.list-card'));
 	        }
 	    }
-	}
-
-	// list title
-	static watchListTitleChanges() {
-		watch (document.querySelectorAll('.list-header h2'), watchers.listTitle);
 	}
 
 }
--- Extension/js/watch.js
+++ Extension/js/watch.js
@@ -1,46 +1,58 @@
-// FIXME should this be a method that returns the appropriate crap instead of a global var?
-var watchers = {
+function getWatcher(key, targets) {
 
-	body : {
-		observer : new MutationObserver(System.detectAndSaveColorBlindFriendlyMode),
-		options : {childList: false, subtree: false, attributes: true, attributeOldValue: true}
-	},
+	var watchers = {
 
-	title : {
-		observer : new MutationObserver(System.processPageOnTitleChange),
-		options : {characterData: true, childList: true, subtree: false}
-	},
+		body : {
+			targets: document.body,
+			observer : new MutationObserver(System.detectAndSaveColorBlindFriendlyMode),
+			options : {childList: false, subtree: false, attributes: true, attributeOldValue: true}
+		},
 
-	board : {
-		observer: new MutationObserver(System.checkForNewLists),
-		options : {childList: true, subtree: false}
-	},
+		title : {
+			targets : document.querySelector('title'),
+			observer : new MutationObserver(System.processPageOnTitleChange),
+			options : {characterData: true, childList: true, subtree: false}
+		},
 
-	list : {
-		observer: new MutationObserver(System.checkForNewCards),
-		options : {childList: true, subtree: false}
-	},
+		board : {
+			targets : document.getElementById('board'),
+			observer: new MutationObserver(System.checkForNewLists),
+			options : {childList: true, subtree: false}
+		},
 
-	listTitle : {
-		observer : new MutationObserver(ListHighlighter.highlight),
-		options : {childList: true, subtree: false}
-	},
+		list : {
+			targets : targets || document.querySelectorAll('.list-cards'),
+			observer: new MutationObserver(System.checkForNewCards),
+			options : {childList: true, subtree: false}
+		},
 
-	listCardTitle : {
-		observer : new MutationObserver(Card.processListCardTitle),
-		options : {characterData: true, childList: true, subtree: false}
-	}
+		listTitle : {
+			targets : document.querySelectorAll('.list-header h2'),
+			observer : new MutationObserver(ListHighlighter.highlight),
+			options : {childList: true, subtree: false}
+		},
 
-};
-
-function watch(targets, watcher) {
-
-	if (targets instanceof NodeList || Array.isArray(targets)) {
-		for (let i = targets.length - 1; i > -1; i--) {
-			watcher.observer.observe(targets[i], watcher.options);
+		listCardTitle : {
+			targets : targets || document.querySelectorAll('.list-card-title'),
+			observer : new MutationObserver(Card.processListCardTitle),
+			options : {characterData: true, childList: true, subtree: false}
 		}
-	} else if (targets instanceof HTMLElement) {
-		watcher.observer.observe(targets, watcher.options);
+
+	};
+
+	return watchers[key];
+}
+
+function watch(watcherKey, targets) {
+
+	var watcher = getWatcher(watcherKey, targets);
+
+	if (watcher.targets instanceof NodeList || Array.isArray(watcher.targets)) {
+		for (let i = watcher.targets.length - 1; i > -1; i--) {
+			watcher.observer.observe(watcher.targets[i], watcher.options);
+		}
+	} else if (watcher.targets instanceof HTMLElement) {
+		watcher.observer.observe(watcher.targets, watcher.options);
 	}
 
 }
