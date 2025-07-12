6b9861372cc879ac9aa81c82a3aba910c03d7ba8
Mark Kenny <beingmrkenny@gmail.com>
2025-07-12 23:37:18 +0100

    Comments to identify what elements are targeted in styles

--- Extension/js/front-end/TrelloPage.js
+++ Extension/js/front-end/TrelloPage.js
@@ -136,39 +136,63 @@ class TrelloPage {
 	static getObfuscatedClassNameFromPlain(className) {
 		let obfuscated;
 		switch (className) {
-			case 'card-name':
-				obfuscated = 'NdQKKfeqJDDdX3';
+			case 'lists':
+				// ol#board, contains all the li.list-wrapper
+				// obfuscated = 'KduRxx1lW4ExLH';
+				// zj2JnDaAFGfZcN _3zcUgV3VaWFBs4
 				break;
-			case 'list-card-drop-preview':
-				obfuscated = 's84vHdvXUJLQdS';
-				break;
-			case 'list-card':
-				obfuscated = 'T9JQSaXUsHTEzk';
-				break;
-			case 'list-cards':
-				obfuscated = 'RD2CmKQFZKidd6';
-				break;
-			case 'list-edit-menu-button':
-				obfuscated = 'bxgKMAm3lq5BpA';
-				break;
-			case 'list-header':
-				obfuscated = 'bPNGI_VbtbXQ8v';
-				break;
-			case 'list-name':
-				obfuscated = 'KLvU2mDGTQrsWG';
-				break;
+
 			case 'list-wrapper':
-				obfuscated = 'bi0h3HALKXjfDq';
+				// <li> element, parent for each list
+				// obfuscated = 'bi0h3HALKXjfDq';
+				obfuscated = 'Iq1byT5o1KoAi6';
 				break;
 			case 'list':
-				obfuscated = 'Sb_QqNKeadm2oq';
+				// <div>, first child of .list-wrapper
+				// obfuscated = 'Sb_QqNKeadm2oq';
+				obfuscated = 'cYogZfEaHc9LfO';
 				break;
-			case 'lists':
-				// ol#board
-				obfuscated = 'KduRxx1lW4ExLH';
+
+			case 'list-header':
+				// <div> element, the parent for the list header
+				// obfuscated = 'bPNGI_VbtbXQ8v';
+				obfuscated = 'aqP9FqumyLvTg5';
+				break;
+			case 'list-name':
+				// <h2> element, button > span > "list header text"
+				obfuscated = 'b4Keujk7X4O23A';
+				break;
+			case 'list-edit-menu-button':
+				// <button> in the top right corner of each list, content ultimately is an ellipsis
+				// bfVCcMmB0Iiute ybVBgfOiuWZJtD mUpWqmjL4CZBvn Yt_v_LmarJM9ZS _St8_YSRMkLv07
+				// obfuscated = 'bxgKMAm3lq5BpA';
+				break;
+
+			case 'list-cards':
+				// <ol> element which contains the cards
+				// B27wN5INCjrQTV LcFnJFfQdwDOsP
+				// obfuscated = 'RD2CmKQFZKidd6';
+				break;
+
+			case 'list-card':
+				// <li> element which ultimately contains the card
+				// obfuscated = 'T9JQSaXUsHTEzk';
+				obfuscated = 'csCxU7M5teCNNl';
+				break;
+			case 'card-name':
+				// <a> element which contains the card text
+				// obfuscated = 'NdQKKfeqJDDdX3';
+				obfuscated = 'Ns0Sj0nEGMQ7un';
+				break;
+
+			case 'list-card-drop-preview':
+				// TBD
+				// obfuscated = 's84vHdvXUJLQdS';
 				break;
 			case 'quick-card-editor-button':
-				obfuscated = 'o0opcYgoysGMA4';
+				// The edit <button> that appears when you hover over the card
+				// Y6PO3DDdkP2fnA ybVBgfOiuWZJtD Yt_v_LmarJM9ZS _St8_YSRMkLv07
+				// obfuscated = 'o0opcYgoysGMA4';
 				break;
 		}
 		return obfuscated;
